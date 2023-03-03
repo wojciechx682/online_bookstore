8b21a68e8332bcb8574ef440c5f98f246b32c81b
Jakub Wojciechowski <jakub.wojciechowski.682@gmail.com>
2023-03-03 04:22:41 +0100

    refaktoryzacja kodu

--- change_quantity.js
+++ change_quantity.js
@@ -1,30 +1,36 @@
 
+function increase(id_ksiazki) {
 
+	// let el = document.getElementById("koszyk_ilosc" + id_ksiazki);
 
-function increase(id_ksiazki)
-{	
-	var el = document.getElementById("koszyk_ilosc"+id_ksiazki);
+	// `${variable}`   <--- template literals.
+	// document.querySelector(`#my-element-${id}`);
+
+	let el = document.querySelector(`#koszyk_ilosc${id_ksiazki}`);
 
 	el.value = parseInt(el.value) + 1;
 
-	var form = document.getElementById("change_quantity_form"+id_ksiazki).submit();		
+	//let form = document.getElementById("change_quantity_form" + id_ksiazki);
+
+	let form = document.querySelector(`#change_quantity_form${id_ksiazki}`)
+
+	form.submit();
 }
 
+function decrease(id_ksiazki) {
 
-function decrease(id_ksiazki)
-{
-	
+	//let el = document.getElementById("koszyk_ilosc" + id_ksiazki);
 
-	var el = document.getElementById("koszyk_ilosc"+id_ksiazki);
+	let el = document.querySelector(`#koszyk_ilosc${id_ksiazki}`);
 
-	if(el.value > 1)
+	if (el.value > 1)
 	{
 		el.value = parseInt(el.value) - 1;
 	}
 
-	var form = document.getElementById("change_quantity_form"+id_ksiazki).submit();	
-
-	//console.log(el.value);
-	
+	let form = document.querySelector(`#change_quantity_form${id_ksiazki}`);
+	form.submit();
 }
+
+
 
