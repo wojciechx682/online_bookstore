4559ab32d849034a487ab300ccb6dee6000f3a7d
Jakub Wojciechowski <jakub.wojciechowski.682@gmail.com>
2023-03-04 22:33:38 +0100

    Update ___test.php

--- test/___test.php
+++ test/___test.php
@@ -1,2 +1,81 @@
+<style>
+    .login-container {
+        max-width: 400px;
+        margin: 0 auto;
+        padding: 20px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        background-color: #f5f5f5;
+    }
+
+    h2 {
+        text-align: center;
+        margin-top: 0;
+    }
+
+    form {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .form-group {
+        margin-bottom: 10px;
+    }
+
+    label {
+        font-weight: bold;
+    }
+
+    input {
+        padding: 5px;
+        border-radius: 5px;
+        border: 1px solid #ccc;
+    }
+
+    button[type="submit"] {
+        padding: 10px;
+        border-radius: 5px;
+        border: none;
+        background-color: #007bff;
+        color: #fff;
+        cursor: pointer;
+    }
+
+    button[type="submit"]:hover {
+        background-color: #0069d9;
+    }
 
 
+</style>
+
+<div class="login-container">
+
+
+    <h2>Login</h2>
+
+
+
+    <form action="login.php" method="post">
+
+
+
+
+        <div class="form-group">
+
+
+
+
+            <label for="username">Username:</label>
+
+
+
+
+            <input type="text" id="username" name="username" required>
+        </div>
+        <div class="form-group">
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+        </div>
+        <button type="submit">Log In</button>
+    </form>
+</div>
