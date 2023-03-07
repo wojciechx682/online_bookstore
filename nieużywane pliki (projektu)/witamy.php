17e3c219dd9e40770c0dd758722d81235bcb399a
Jakub Wojciechowski <jakub.wojciechowski.682@gmail.com>
2023-03-08 00:08:33 +0100

    Update witamy.php

--- nieużywane pliki (projektu)/witamy.php
+++ nieużywane pliki (projektu)/witamy.php
@@ -71,7 +71,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<title>Księgarnia internetowa</title>
-	<link rel="stylesheet" href="nieużywane pliki (projektu)/style.css">
+	<link rel="stylesheet" href="style.css">
 </head>
 
 <body>
@@ -80,9 +80,9 @@
 
 	Tylko martwi ujrzeli koniec wojny  Platon<br><br>
 	
-	<a href="index.php">Zaloguj się na swoje konto!</a> <br><br>
+	<a href="../index.php">Zaloguj się na swoje konto!</a> <br><br>
 	
-	<form action="zaloguj.php" method="post">
+	<form action="../zaloguj.php" method="post">
 	
 		Login: <br> <input type="text" name="login"> <br>
 		Hasło: <br> <input type="password" name="haslo"> <br><br>
