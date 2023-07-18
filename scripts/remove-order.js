51f336ed6aa30b8c1ceca110ff57b40d56685309
Jakub Wojciechowski <jakub.wojciechowski.682@gmail.com>
2023-07-18 23:41:49 +0200

    refactor remove-order.js

--- scripts/remove-order.js
+++ scripts/remove-order.js
@@ -2,270 +2,117 @@
 // order.php - usunięcie / zarchwizowanie / zamknięcie zamówienia
     // admin podaje powód (notatkę) - którą potem może wyświetlić klient;
 
-function removeOrder(orderId) { // po kliknięciu przycisku "Archiwizuj"
+function removeOrder(orderId) { // po kliknięciu przycisku "Archiwizuj";    orderId -> "1130";
 
-                // Refactor - zamiana na tylko jedno okno Archiwizowania zamówienia ;
+// Refactor - zamiana na tylko jedno okno Archiwizowania zamówienia ;
 
-                console.log("\n orderId --> ", orderId); // "1130" ;
+    // let removeBox = document.getElementById("update-status");
+    // let removeBox = document.querySelector(".update-status"+orderId);
+    let removeBox = document.querySelector(".update-status");
+        // pojawienie się okienka po kliknięciu "Zarchiwizuj";
+    console.log("\nremoveBox (okno archiwizowania zamówienia) --> ", removeBox);
 
-                        // let removeBox = document.getElementById("update-status"); // pojawienie się okenka po kliknięciu "Usuń";
-                //let removeBox = document.querySelector(".update-status"+orderId);
-                let removeBox = document.querySelector(".update-status");
-                    // pojawienie się okenka po kliknięciu "Zarchiwizuj";
+    // dodanie atrybutu "id" (order-id) do pól formularza (input type="hidden") -->
 
-                // console.log("\n orderId --> ", removeBox); // <div class="update-status1035 hidden"> - OKIENKO PO KLIKNIĘCIU "ARCHIWIZUJ";
-
-                console.log("\nremoveBox (okno archiwizowania zamówienia) --> ", removeBox);
-
-// dodanie atrybutu "id" (order-id) do pól formularza (input type="hidden") -->
-
-    let input = removeBox.querySelector('form.remove-order > input[type="hidden"]'); // input type="hidden"
-        console.log('\n\n input --> ', input);
+    let input = removeBox.querySelector('form.remove-order > input[type="hidden"]'); // input type="hidden";
     input.value = orderId;
 
-        // removeBox.querySelector('form.remove-order button[type="submit"]');
+    /*if(removeBox.classList.contains("hidden")) {
+        removeBox.classList.toggle("hidden");
+    }*/
 
-                /*if(removeBox.classList.contains("hidden")) {
-                    removeBox.classList.toggle("hidden");
-                }*/
+    removeBox.classList.toggle("hidden"); // pojawienie się okenka po kliknięciu "Usuń";
 
-                removeBox.classList.toggle("hidden");
+    let mainContainer = document.getElementById("main-container");
 
-                            //removeBox.classList.toggle("hidden");
-                            /*removeBox.style.filter = "inherit";*/
-                            //removeBox.classList.toggle('normal');
+    /*if( ! mainContainer.classList.contains("bright")) {
+        mainContainer.classList.toggle("bright");
+    }*/
 
-                let mainContainer = document.getElementById("main-container");
+    mainContainer.classList.toggle("bright");
+    mainContainer.style.pointerEvents = "none";
+    mainContainer.style.userSelect = "none";
 
-                /*if( ! mainContainer.classList.contains("bright")) {
-                    mainContainer.classList.toggle("bright");
-                }*/
+    //let icon = document.querySelector(".icon-cancel" + orderId);
+    let icon = document.querySelector(".icon-cancel");  // ikona "✖"
+    //let cancelBtn = document.querySelector(".cancel-order"+orderId);
+    let cancelBtn = document.querySelector(".cancel-order"); // przycisk "Anuluj"
 
-                mainContainer.classList.toggle("bright");
-            mainContainer.style.pointerEvents = "none";
-            mainContainer.style.userSelect = "none";
+        console.log("\nicon --> ", icon);
+        console.log("\ncancelBtn --> ", cancelBtn);
 
+    // zamknięcie okna po kliknięciu "✖";
 
-                //mainContainer.classList.toggle("bright");
+    icon.addEventListener("click", function() {
+        closeRemoveBox();
+    });
 
-                        //let icon = document.querySelector(".icon-cancel" + orderId);
-                let icon = document.querySelector(".icon-cancel");  // ikona "✖"
+    // zamknięcie okna po kliknięciu "Anuluj";
 
-                        //let cancelBtn = document.querySelector(".cancel-order"+orderId);
-                let cancelBtn = document.querySelector(".cancel-order"); // przycisk "Anuluj"
+    cancelBtn.addEventListener("click", function() {
+        closeRemoveBox();
+    });
 
+    function closeRemoveBox() {
 
-                    console.log("\nicon --> ", icon);
-                    console.log("\ncancelBtn --> ", cancelBtn);
+        mainContainer.removeAttribute("style");
+        let textarea = removeBox.querySelector("textarea"); // <textarea> w tym okienku (removeBox);
+        resetError(textarea); // usuwa komunikat o błędzie jeśli jest on widoczny;
+        textarea.value = "";
 
-                // zamknięcie okna po kliknięciu "✖";
+        // removeBox.classList.toggle("hidden");
 
-                icon.addEventListener("click", function() {
-                    closeRemoveBox();
-                });
+        /*if(!removeBox.classList.contains("hidden")) { // jeśli okno jest widoczne;
+            removeBox.classList.toggle("hidden");
+        }*/
 
-                cancelBtn.addEventListener("click", function() {
-                    closeRemoveBox();
-                });
-
-
-                /*icon.onclick = function() {
-
-                    mainContainer.removeAttribute("style");
-
-                        //toggleRemoveBox(removeBox, mainContainer);
-                        let textarea = removeBox.querySelector("textarea"); // <textarea> w tym okienku (removeBox);
-                        resetError(textarea); // usuwa komunikat o błędzie jeśli jest on widoczny;
-                        //removeOrder(orderId); // rekurencja -> toggle - tym razem zamknięcie okienka;
-
-                        console.log("\n 42 orderId --> ", orderId);
-
-                        /!*if( ! removeBox.classList.contains("hidden")) {
-                            removeBox.classList.toggle("hidden");
-                        }*!/
-
-                        removeBox.classList.toggle("hidden");
-
-                        /!*if(mainContainer.classList.contains("bright")) {
-                            mainContainer.classList.toggle("bright");
-                        }*!/
-
-                        mainContainer.classList.toggle("bright");
-
-                        let successMsg = removeBox.querySelector("span.archive-success"); // komunikat potwierdzający Zarchiwizowanie zamówienia ;
-                    let errorMsg = removeBox.querySelector("span.update-failed"); // komunikat opisujący błąd po nieudanej próbie wykonania żądania;
+        removeBox.classList.toggle("hidden", true); // ukrycie okna - dodanie klasy "hidden" ;  true - oznacza że klasa zostanie tylko dodana;
 
 
-                    console.log("successMsg -> ", successMsg);
+        // mainContainer.classList.toggle("bright");
 
-                    if(successMsg) {            // jeśli element istnieje w kodzie HTML (jeśli "istnieje");
-                        successMsg.remove();
+        /*if(mainContainer.classList.contains("bright")) { // jeśli tło jest ciemne
+            mainContainer.classList.toggle("bright");
+        }*/
 
-                            //let confirmButton = $('form.remove-order button[type="submit"]');
-                            let confirmButton = removeBox.querySelector('form.remove-order button[type="submit"]');
-                            //let cancelButton = $('button.cancel-order');
-                            let cancelButton = removeBox.querySelector('button.cancel-order');
-                            //let textarea = $('textarea[name="comment"]');
-                            //let textarea = removeBox.querySelector('textarea[name="comment"]');
-                            console.log("confirmButton -> ", confirmButton);
-                            console.log("cancelButton -> ", cancelButton);
-                            //confirmButton.css("display", "block");
-                            confirmButton.style.display = "initial";
-                            //cancelButton.css("display", "block");
-                            cancelButton.style.display = "initial";
-                            //textarea.val("");
-                    }
-
-                    if(errorMsg) {            // jeśli element istnieje w kodzie HTML (jeśli "istnieje");
-                        errorMsg.remove();
-                        //let confirmButton = $('form.remove-order button[type="submit"]');
-                        let confirmButton = removeBox.querySelector('form.remove-order button[type="submit"]');
-                        //let cancelButton = $('button.cancel-order');
-                        let cancelButton = removeBox.querySelector('button.cancel-order');
-                        //let textarea = $('textarea[name="comment"]');
-                        //let textarea = removeBox.querySelector('textarea[name="comment"]');
-                        console.log("confirmButton -> ", confirmButton);
-                        console.log("cancelButton -> ", cancelButton);
-                        //confirmButton.css("display", "block");
-                        confirmButton.style.display = "initial";
-                        //cancelButton.css("display", "block");
-                        cancelButton.style.display = "initial";
-                        //textarea.val("");
-
-                    }
+        mainContainer.classList.toggle("bright", false); // usunięcie ciemnego tła ; false - oznacza, że klasa zostanie tylko usunięta;
 
 
-                    textarea.value = "";
-
-                    //removeBox.classList.toggle("hidden");
-                    //mainContainer.classList.toggle("bright");
-                }*/
-
-                /* icon.addEventListener("click", function() {
-
-                });*/
-
-                /*cancelBtn.onclick = function() {
-
-                        mainContainer.removeAttribute("style");
-
-                        let textarea = removeBox.querySelector("textarea"); // <textarea> w tym okienku (removeBox);
-                        resetError(textarea); // usuwa komunikat o błędzie jeśli jest on widoczny;
-                        //removeOrder(orderId); // przycisk "Anuluj"
-
-                        console.log("\n 63 orderId --> ", orderId);
-
-                        /!*if(!removeBox.classList.contains("hidden")) {
-                            removeBox.classList.toggle("hidden");
-                        }*!/
-
-                        removeBox.classList.toggle("hidden");
-
-                        /!*if(mainContainer.classList.contains("bright")) {
-                            mainContainer.classList.toggle("bright");
-                        }*!/
-
-                        mainContainer.classList.toggle("bright");
-
-                        let successMsg = removeBox.querySelector("span.archive-success");
-                        console.log("successMsg -> ", successMsg);
-                        let errorMsg = removeBox.querySelector("span.update-failed"); // komunikat opisujący błąd po nieudanej próbie wykonania żądania;
-
-
-                        if(successMsg) {            // jeśli element istnieje w kodzie HTML (jeśli "istnieje");
-                            successMsg.remove();
-                            //let confirmButton = $('form.remove-order button[type="submit"]');
-                            let confirmButton = removeBox.querySelector('form.remove-order button[type="submit"]');
-                            //let cancelButton = $('button.cancel-order');
-                            let cancelButton = removeBox.querySelector('button.cancel-order');
-                            //let textarea = $('textarea[name="comment"]');
-                            //let textarea = removeBox.querySelector('textarea[name="comment"]');
-                            console.log("confirmButton -> ", confirmButton);
-                            console.log("cancelButton -> ", cancelButton);
-                            //confirmButton.css("display", "block");
-                            confirmButton.style.display = "initial";
-                            //cancelButton.css("display", "block");
-                            cancelButton.style.display = "initial";
-                            //textarea.val("");
-                         }
-
-                    if(errorMsg) {            // jeśli element istnieje w kodzie HTML (jeśli "istnieje");
-                        errorMsg.remove();
-                        //let confirmButton = $('form.remove-order button[type="submit"]');
-                        let confirmButton = removeBox.querySelector('form.remove-order button[type="submit"]');
-                        //let cancelButton = $('button.cancel-order');
-                        let cancelButton = removeBox.querySelector('button.cancel-order');
-                        //let textarea = $('textarea[name="comment"]');
-                        //let textarea = removeBox.querySelector('textarea[name="comment"]');
-                        console.log("confirmButton -> ", confirmButton);
-                        console.log("cancelButton -> ", cancelButton);
-                        //confirmButton.css("display", "block");
-                        confirmButton.style.display = "initial";
-                        //cancelButton.css("display", "block");
-                        cancelButton.style.display = "initial";
-                        //textarea.val("");
-                    }
-
-
-                    textarea.value = "";
-                }*/
-
-                /*cancelBtn.addEventListener("click", function() {
-
-                });*/
-
-                function closeRemoveBox() {
-
-                    mainContainer.removeAttribute("style");
-                    // toggleRemoveBox(removeBox, mainContainer);
-                    let textarea = removeBox.querySelector("textarea"); // <textarea> w tym okienku (removeBox);
-                    resetError(textarea); // usuwa komunikat o błędzie jeśli jest on widoczny;
-                    textarea.value = "";
-                    // removeOrder(orderId); // rekurencja -> toggle - tym razem zamknięcie okienka;
-                    // console.log("\n 42 orderId --> ", orderId);
-
-                    if(!removeBox.classList.contains("hidden")) { // jeśli okno jest widoczne
-                        removeBox.classList.toggle("hidden");
-                    }
-                        //removeBox.classList.toggle("hidden");
-
-
-                    if(mainContainer.classList.contains("bright")) { // jeśli tło jest ciemne
-                        mainContainer.classList.toggle("bright");
-                    }
-                        //mainContainer.classList.toggle("bright");
-
-                    removeMessagesAndResetButtons(removeBox);
-
-
-                    //removeBox.classList.toggle("hidden");
-                    //mainContainer.classList.toggle("bright");
-                }
+        removeMessagesAndResetButtons(removeBox);
+    }
 
 
     function removeMessagesAndResetButtons(removeBox) {
 
-        const successMsg = removeBox.querySelector("span.archive-success");
-        const errorMsg = removeBox.querySelector("span.update-failed");
+            /*const successMsg = removeBox.querySelector("span.archive-success");
+            const errorMsg = removeBox.querySelector("span.update-failed");*/
+            const spanMsgs = removeBox.querySelectorAll("span.archive-success, span.update-failed");
         const confirmButton = removeBox.querySelector('form.remove-order button[type="submit"]');
         const cancelButton = removeBox.querySelector('button.cancel-order');
 
-        if (successMsg) {
-            successMsg.remove();
-            confirmButton.style.display = "initial";
+            /*if (successMsg) {
+                successMsg.remove();
+                confirmButton.style.display = "initial";
+            }
+            if (errorMsg) {
+                errorMsg.remove();
+                confirmButton.style.display = "initial";
+            }*/
+
+        // better -> iterates over the collected spanMsgs NodeList using a for loop and removes each span individually if it exists.
+
+        for(let i = 0; i < spanMsgs.length; i++) {
+                let resultMsg = spanMsgs[i];
+            if(resultMsg) {
+                resultMsg.remove();
+                confirmButton.style.display = "initial";
+            }
         }
-        if (errorMsg) {
-            errorMsg.remove();
-            confirmButton.style.display = "initial";
-        }
+
         cancelButton.style.display = "initial";
     }
 
     $('.delivery-date').css("display", "block"); // wyświetlenie formularza (domyślnie był ukryty
-                                                /*let div = document.querySelector(".delivery-date");
-                                                div.classList.toggle("hidden");*/
-
-
-
-
+                                                 // let div = document.querySelector(".delivery-date");
+                                                 // div.classList.toggle("hidden");
 }
