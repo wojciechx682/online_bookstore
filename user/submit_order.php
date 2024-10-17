3ff24c602166df40bc996ed720582fdba8668d4d
Jakub Wojciechowski <jakub.wojciechowski.682@gmail.com>
2024-10-17 12:40:33 +0200

    refactor

--- user/submit_order.php
+++ user/submit_order.php
@@ -31,13 +31,13 @@
                     <h3 id="cart-header">Złóż zamówienie</h3>
 
                     <?php
-                        echo '<pre>';
+                        /*echo '<pre>';
                         var_dump($_SESSION["delivery-types"]);
                         echo '</pre>'; echo "<br>";
 
                         echo '<pre>';
                         var_dump($_SESSION["payment-methods"]);
-                        echo '</pre>'; echo "<br><br>";
+                        echo '</pre>'; echo "<br><br>";*/
 
                         //echo var_dump($_SESSION["delivery-types"]); echo "<br>";
 
