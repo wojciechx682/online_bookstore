2ff31b0de7df47291bb6b8464ac80c004a39012d
Jakub Wojciechowski <jakub.wojciechowski.682@gmail.com>
2024-11-30 13:52:42 +0100

    refactor

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 Online bookstore web application Project
 
-![Logo projektu](./online_bookstore.png)
+<h3>How to install project on localhost:</h3>
+
+1. Install XAMPP
+2. Open phpmyadmin (http://localhost:8080/phpmyadmin/)
+3. Create new database named "bookstore" with "utf8_polish_ci" encoding
+4. Import database from "bookstore.sql" into "bookstore" database
+
+Main page:
+![Logo projektu](./assets/online_bookstore.png)
+
+Book page:
+![Logo projektu](./assets/online_bookstore3.png)
+
+Admin panel:
+![Logo projektu](./assets/online_bookstore2.png)
+
+![Logo projektu](./assets/online_bookstore4.png)
+
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'jakub/online_bookstore',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1f270d381597b77fc774146b37f99b9ff8060d79',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jakub/online_bookstore' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1f270d381597b77fc774146b37f99b9ff8060d79',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.8.0',
+            'version' => '6.8.0.0',
+            'reference' => 'df16b615e371d81fb79e506277faea67a1be18f1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
