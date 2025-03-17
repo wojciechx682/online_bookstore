68f639e9b814183b4a1af4d2e1e6c9e55209b504
Jakub Wojciechowski <59968221+wojciechx682@users.noreply.github.com>
2025-03-17 16:47:28 +0100

    refactor

--- authenticate-user.php
+++ authenticate-user.php
@@ -2,13 +2,8 @@
     // check if user is logged-in, and user-type is "client" - if not, redirect to login page;
     require_once "start-session.php";
 
-    if ( !isset($_SESSION['zalogowany']) ||
-         !isset($_SESSION['id']) ||
-           $_SESSION['zalogowany'] !== true ||
-           $_SESSION["user-type"] !== "klient" ) {
-        // - nie jesteśmy zalogowani,
-            // lub
-        // - typ usera to nie klient (mimo że jest zalogowany)
+    if (!isset($_SESSION['zalogowany']) || !isset($_SESSION['id']) || $_SESSION['zalogowany'] !== true || $_SESSION["user-type"] !== "klient") {
+        // nie jesteśmy zalogowani lub typ usera to nie klient (mimo że jest zalogowany)
         $_SESSION["login-error"] = true;
             header("Location: zaloguj.php");
                 exit();
